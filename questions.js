9524574f92281cd5eb27c1e518cdd48131abd705
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-01-21 04:38:22 -0600

    displaying questions

---
+++ questions.js
@@ -0,0 +1,7 @@
+var questions = [
+    {
+        title: "How would you refer to the 'font-size' property in javascript?",
+        choices: ["font-size", "fontSize", "font.size", "$(font-size)"],
+        answer: "fontSize"
+    }
+]
--- script.js
+++ script.js
@@ -18,19 +18,87 @@ var playAreaEl = document.getElementById("play-area");
 // - If user clicks 'Go Back' button, show the start screen
 // - If user clicks 'Clear Highscores' button, clear the high scores and update the screen
 
-function showStartScreen () {
-    // Show header, description, and start button
+/*
+playAreaEl.addEventListener("click", function(event) {
+    var element = event.target;
+
+    // Handle button clickss
+    if (element.matches("button")) {
+        var buttonId = element.getAttribute("data-id");
+
+        // switch (element.textContent) {
+        switch (buttonId) {
+            case "start-quiz":
+                // alert("You pressed the start button!");
+        }
+    }
+}) */
+
+function runQuiz() {
+    var answerDiv = document.createElement("div");
+
+    function getQuestion() {
+        var questionEl = document.createElement("h3");
+        questionEl.textContent = questions[0].title;
+        playAreaEl.append(questionEl);
+        playAreaEl.append(answerDiv);
+
+        for (var k = 0; k < questions[0].choices.length; k++) {
+            var answerEl = document.createElement("div");
+            answerEl.textContent = questions[0].choices[k];
+            answerEl.setAttribute("data-id", k);
+
+            answerDiv.append(answerEl);
+        }
+    }
+
+    answerDiv.addEventListener("click", function(event) {
+        var element = event.target;
+
+        // 
+    })
+
+    // Randomly pull new question from list of available questions
+
+    // At first, pull just first question and display it
+    // var questionEl = document.createElement("h3");
+    // questionEl.textContent = questions[0].title;
+    // playAreaEl.append(questionEl);
+
+    // for (var k = 0; k < questions[0].choices.length; k++) {
+    //     var answerEl = document.createElement("div");
+    //     answerEl.textContent = questions[0].choices[k];
+
+    //     playAreaEl.append(answerEl);
+    // }
+
+    // Clear the screen
+    $("#play-area").empty();
+
+    // Pull the first question
+    getQuestion();
+} 
+
+// Display the start screen
+function showStartScreen() {
+    // Elements
     var headerEl = document.createElement("h1");
     var descriptionEl = document.createElement("p");
     var startQuizButton = document.createElement("button");
+
+    // Event listener for start button
+    startQuizButton.addEventListener("click", function() {
+        runQuiz();
+    })
 
     headerEl.textContent = "Coding Quiz Challenge";
     descriptionEl.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
     startQuizButton.textContent = "Start Quiz";
 
+    // Show header, description, and start button
     playAreaEl.append(headerEl);
     playAreaEl.append(descriptionEl);
-    playAreaEl.append(startQuizButton);
+    playAreaEl.append(startQuizButton); 
 }
 
 // On initial page load, show start screen
